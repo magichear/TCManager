bf09c512f80eb28168363281af73ee901745087e
Magichear <wgdecade43@mail.ustc.edu.cn>
2025-05-15 11:47:16 +0800

    [Processing] 各大控制器已引入日志功能

--- src/main/java/com/magichear/TCManager/utils/EnumTypeHandler.java
+++ src/main/java/com/magichear/TCManager/utils/EnumTypeHandler.java
@@ -14,6 +14,7 @@ import java.sql.SQLException;
  */
 public abstract class EnumTypeHandler<E extends Enum<E>> extends BaseTypeHandler<E> {
 
+    @SuppressWarnings("unused")
     private final Class<E> type;
 
     /**
